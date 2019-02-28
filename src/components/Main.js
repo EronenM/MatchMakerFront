1ee293c8b6455a045e21cb2e8d7f997773348a30
aarni-k <karjalainen.aarni@gmail.com>
2019-02-28 17:04:08 +0200

    string toimiva

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SelectRegUser from './SelectRegUser';
 import Button from 'react-bootstrap/Button';
 import { NavLink } from 'react-router-dom';
-import LogInLanding from '../LogInLanding.js';
+import LogInLanding from './LogInLanding';
 import './Main.css';
 
 class Main extends Component {
